d5485201021fbb9e0f084381d187ba8dda5587c2
Yogesh Sharma <yogeshatworld3.14@gmail.com>
2023-03-07 16:49:02 +0530

    ReUploading files

--- CRS-Signify-Group-A-POS-Documentation/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.signify.bean"},{"l":"com.signify.client"},{"l":"com.signify.constants"},{"l":"com.signify.dao"},{"l":"com.signify.exception"},{"l":"com.signify.helper"},{"l":"com.signify.service"},{"l":"com.signify.utils"},{"l":"helper"}];updateSearchResults();
--- CRS-Signify-SpringRest-Group-A/src/main/java/com/signify/dao/UserDAOInterface.java
+++
@@ -1,7 +0,0 @@
-package com.signify.dao;
-import com.signify.exception.UserNotFoundException;
-
-public interface UserDAOInterface {
-	public String[] login(String username, String password) throws UserNotFoundException;
-    public boolean updatePassword(String username,String oldPassword,String newPassword) throws UserNotFoundException;
-}
