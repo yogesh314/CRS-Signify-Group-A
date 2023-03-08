6dd115fef04fd97b6adf1e9c74c1e0f84c0d626d
Yogesh Sharma <yogeshatworld3.14@gmail.com>
2023-03-08 12:04:26 +0530

    Rename

--- CRS-Signify-SpringRest-Group-A/src/main/java/com/signify/service/UserInterface.java
+++
@@ -1,9 +0,0 @@
-package com.signify.service;
-
-public interface UserInterface {
-	public String[] login(String username, String password);
-	public void logout();
-	public void updateDetails();	
-	public boolean updatePassword(String username, String oldPassword, String newPassword);
-	
-}
